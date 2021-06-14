8f5f88cc81e83b052eb73938b0cbe23dc67e8e68
jonrosebud <jonathanrose94@gmail.com>
2021-06-14 14:36:03 -0700

    Add files via upload
    note

--- waypoint_path.py
+++ waypoint_path.py
@@ -28,84 +28,87 @@ if debugging == True:
     print(position_desired)
     
     
-while (not(math.isclose(position_desired[1], position_actual[1], rel_tol = 0.1) or math.isclose(position_desired[0], position_actual[0], rel_tol = 0.1))):
+while (not(math.isclose(position_desired[1], position_actual[1], abs_tol = tolerance) and math.isclose(position_desired[0], position_actual[0], abs_tol = tolerance))):
+    
+    position_actual[0] = int(input('position_actual[0] '))
+    position_actual[1] = int(input('position_actual[1] '))
     
     # w
-    if position_desired[1] > position_actual[1]:
+    if position_desired[1] > position_actual[1] and not math.isclose(position_desired[1], position_actual[1], abs_tol = tolerance):
         if w == True:
             if debugging == True:
-                print(w)
-            continue
+                print('w ',w)
+            pass # do nothing
         else:
             w = True
             if debugging == True:
-                print(w)
+                print('w ',w)
             if debugging == False:
                 pydirectinput.keyDown('w') # up
     else:
         w = False
         if debugging == True:
-            print(s)
+            print('w ',w)
         if debugging == False:
             pydirectinput.keyUp('w')
     
     
     # s
-    if position_desired[1] < position_actual[1]:
+    if position_desired[1] < position_actual[1] and not math.isclose(position_desired[1], position_actual[1], abs_tol = tolerance):
         if s == True:
             if debugging == True:
-                print(s)
-            continue
+                print('s ',s)
+            pass # do nothing
         else:
             s = True
             if debugging == True:
-                print(s)
+                print('s ',s)
             if debugging == False:
                 pydirectinput.keyDown('s') # down
     else:
          s = False
          if debugging == True:
-             print(s)
+             print('s ',s)
          if debugging == False:
              pydirectinput.keyUp('s') # down
         
         
     # q                       
-    if position_desired[0] < position_actual[0]:
+    if position_desired[0] < position_actual[0] and not math.isclose(position_desired[0], position_actual[0], abs_tol = tolerance):
         if q == True:
             if debugging == True:
-                print(q)
-            continue
+                print('q ',q)
+            pass # do nothing
         else:
             q = True
             if debugging == True:
-                print(q)
+                print('q ',q)
             if debugging == False:
                 pydirectinput.keyDown('q') # left
     else:
         q = False
         if debugging == True:
-            print(q)
+            print('q ',q)
         if debugging == False:
             pydirectinput.keyUp('q') # left
         
     
     # e
-    if position_desired[0] > position_actual[0]:
+    if position_desired[0] > position_actual[0] and not math.isclose(position_desired[0], position_actual[0], abs_tol = tolerance):
         if e == True:
             if debugging == True:
-                print(e)
-            continue
+                print('e ',e)
+            pass # do nothing
         else:
             e = True
             if debugging == True:
-                print(e)
+                print('e ',e)
             if debugging == False:
                 pydirectinput.keyDown('e') # right
     else:
         e = False
         if debugging == True:
-            print(e)
+            print('e ',e)
         if debugging == False:
             pydirectinput.keyUp('e') # right
             
